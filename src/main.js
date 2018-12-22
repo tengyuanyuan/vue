4701c73b25bd2fc4dc8f3caa55a6a1561cbfc21f
tengyuanyuan <2575339643@qq.com>
2018-12-22 14:35:36 +0800

    修改了readme和main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 //console.log('ok');
 import Vue from 'vue'
 //导入mui样式
-import './lib/mui/css/mui.min.css'
+import './lib/mui/css/mui.min.css'  
 //按需导入mint-ui组件
 import {Header} from 'mint-ui'
 Vue.component(Header.name,Header)
